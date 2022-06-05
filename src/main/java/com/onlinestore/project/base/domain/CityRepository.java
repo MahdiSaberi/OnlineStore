7e9543663e9f3ef246626a0ffe880ad39df4e287
mahdisaberi <mahdisaberi35@gmail.com>
2022-06-05 15:56:22 +0430

    body updated

--- src/main/java/com/onlinestore/project/base/domain/CityRepository.java
+++ src/main/java/com/onlinestore/project/base/domain/CityRepository.java
@@ -1,4 +1,14 @@
 package com.onlinestore.project.base.domain;
 
-public class CityRepository {
+import com.onlinestore.project.base.repository.imlp.BaseRepositoryImpl;
+
+import javax.persistence.EntityManagerFactory;
+
+public class CityRepository extends BaseRepositoryImpl<Object, Category> {
+
+    public CityRepository(EntityManagerFactory emf){super(emf);}
+
+    public void initCity(City city){
+        add(city);
+    }
 }
